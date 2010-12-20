ef9e02447dc8980ca5d241c8dc7d0f54368bf33c
Dan Newman <dnewms@gmail.com>
2010-12-20 18:57:25 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viddejour::Application.config.secret_token = '3754274834270e22fb9cfeb46cc5cba4fd200ce000e38d563f69e3ba2a596037c3210131fa926afb36859ecb7d075cc0638efdfaf3d7e8b4677b63630dd69aad'
